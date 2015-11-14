f2f51b11be55c06868dff03da230ea32073050bf
trevorlewis <trevorlewis13@gmail.com>
2015-11-14 12:17:07 -0800

    updated preformat script to work with large files

--- Text.jl/mytest/data/preformat.sh
+++ Text.jl/mytest/data/preformat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-while read line; do
-	perl -pe 's/\n/ / if $_ == /<\/doc>/' $line"-text.xml" > $line"-text-format.xml"
+while read lang; do
+	perl -pe 's/\n/ / if !($_ =~ m/<\/doc>/)' $lang"-text.xml" | sed 's/<doc id=\"//' | sed 's/\" url=\"/\t/' | sed 's/\" title=\"/\t/' | sed 's/\">\s*/\t/' | sed 's/\s*<\/doc>//' | sed 's/^/'$lang'\t/' > $lang"-text-format.xml"
 done < list.txt
