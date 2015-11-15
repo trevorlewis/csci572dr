feb5a79737e85d6bc12133d5edb4861845a90648
trevorlewis <trevorlewis13@gmail.com>
2015-11-14 18:00:47 -0800

    updated format.sh to increase speed

--- Text.jl/mytest/data/format.sh
+++ Text.jl/mytest/data/format.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# preprocess the plain text files generated using WikiExtractor
-# by combining random 'n' lines of all the files into a single TSV file
-# such that the first column contains the language of the text in the second column
-
 file="test.tsv"
 num=1000
 for i in "$@"
@@ -24,20 +20,31 @@ done
 rm -f $file
 
 while read lang; do
-	infile=$lang"-text-format.xml"
+    infile=$lang"-text-format.xml"
+    array=()
     lc=$(wc -l < $infile)
-    for n in `seq 1 $num`; do
-        # get a random number between 1 and $lc
+    while [ ${#array[@]} -lt $num ]; do
         rnd=$RANDOM
         let "rnd %= $lc"
-        ((rnd++))
-        # traverse file and find line number $rnd
-        i=0
-        while read -r line; do
-         ((i++))
-         [ $i -eq $rnd ] && break
-        done < $infile
-        # output random line
-        echo $line >> $file
+        if [[ " ${array[*]} " == *" $rnd "* ]]; then
+			continue
+		fi
+		array+=($rnd)
     done
+	sorted=$(echo ${array[@]} | tr ' ' '\n' | sort -nu | tr '\n' ' ')
+    array=()
+    for i in "${sorted[@]}"; do
+		array+=($i)
+	done
+	i=0
+	p=0
+	while read -r line; do
+		if [ $i -eq ${array[$p]} ]; then
+			echo $line >> $file
+			((p++))
+			[ $p -eq ${#array[@]} ] && break
+		fi
+		((i++))
+	done < $infile
+
 done < list.txt
