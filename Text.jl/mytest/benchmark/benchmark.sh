b8048e6ad5805102febf357cc83279228a3aebfa
trevorlewis <trevorlewis13@gmail.com>
2015-11-20 18:24:03 -0800

    Added script to benchmark Text.jl using command line

--- Text.jl/mytest/benchmark/benchmark.sh
+++ Text.jl/mytest/benchmark/benchmark.sh
@@ -2,3 +2,5 @@
 
 javac -cp ".:tika-app-1.11.jar:json-simple-1.1.1.jar" Benchmark.java
 java -cp ".:tika-app-1.11.jar:json-simple-1.1.1.jar" Benchmark ../data/test.tsv > result.tsv
+
+./benchmark_text_cl.sh ../data/test.tsv > result2.tsv
---
+++ Text.jl/mytest/benchmark/benchmark_text_cl.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+file=$1 # first argument input file
+url="http://localhost:8000"  # Text.jl LID HTTP Server's URL
+delimiter=$'\t' # delimiter tab
+
+while IFS=$delimiter read -ra line; do
+	startTime=`date +%s%N`
+	json=`curl -s -X PUT -d "${line[4]}" $url`
+	endTime=`date +%s%N`
+
+	result=$(echo $json| cut -d'"' -f 4)
+	let duration=$endTime-$startTime
+	echo "${line[1]}$delimiter${line[0]}$delimiter$result$delimiter$duration"
+done < $file
