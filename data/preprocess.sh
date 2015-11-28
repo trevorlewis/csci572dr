db057049efec1dda5402e291f9cc8dd2883dabea
trevorlewis <trevorlewis13@gmail.com>
2015-11-27 23:01:08 -0800

    preprocess.sh code cleanup

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -1,15 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # preprocess the plain text files generated using WikiExtractor
 # by combining random 'n' lines of all the files into a single TSV file
 # such that the first column contains the language of the text in the second column
 
+# Usage:
+#     preprocess.sh [options]
+# Options:
+#     -l, --lang : languages file name
+#     -f, --file : output file name
+#     -n, --num : number of lines for each file
+# Example:
+#     preprocess.sh -l=languages.txt -f=text.tsv -n=10000
+
+languages="./languages.txt"
 file="text.tsv"
 num=1000
+
 for i in "$@"
 do
 case $i in
-	-f=*|--file=*)
+    -l=*|--lang=*)
+    languages="${i#*=}"
+    ;;
+    -f=*|--file=*)
     file="${i#*=}"
     ;;
     -n=*|--num=*)
@@ -23,8 +37,8 @@ done
 
 rm -f $file
 
-while read line; do
-	perl -pe 's/<[^>]*>//g' $line"-text.xml" | sed '/^\s*$/d' | sed 's/^/'$line'\t/' > $line"-text.tmp"
-	shuf -n $num $line"-text.tmp" >> $file
-	rm -f $line"-text.tmp"
-done < list.txt
+while read lang; do
+    perl -pe 's/<[^>]*>//g' $lang"-text.xml" | sed '/^\s*$/d' | sed 's/^/'$lang'\t/' > $lang"-text.tmp"
+    shuf -n $num $lang"-text.tmp" >> $file
+    rm -f $lang"-text.tmp"
+done < $languages
